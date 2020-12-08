b79d08e42374d5b4c155e8a00e23a8dfa01cc988
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-08 08:45:00 +0100

    create note repository

---
+++ src/main/java/xyz/stasiak/noteapp/model/NoteRepository.java
@@ -0,0 +1,9 @@
+package xyz.stasiak.noteapp.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface NoteRepository extends JpaRepository<Note, Integer> {
+
+}
