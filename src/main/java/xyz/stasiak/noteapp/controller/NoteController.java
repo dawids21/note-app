a4be6035b580f0ccc4aefd781fb32887bac47674
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-15 08:21:29 +0100

    add endpoints to my controller

--- src/main/java/xyz/stasiak/noteapp/controller/NoteController.java
+++ src/main/java/xyz/stasiak/noteapp/controller/NoteController.java
@@ -1,6 +1,14 @@
 package xyz.stasiak.noteapp.controller;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import xyz.stasiak.noteapp.model.Note;
 import xyz.stasiak.noteapp.model.NoteRepository;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
 
 public class NoteController {
 
@@ -8,5 +16,44 @@ public class NoteController {
 
     public NoteController(NoteRepository noteRepository) {
         this.noteRepository = noteRepository;
+    }
+
+    @GetMapping(path = "/notes", params = {"!size", "!sort", "!page"})
+    public ResponseEntity<List<Note>> getAllNotes() {
+        return ResponseEntity.ok(noteRepository.findAll());
+    }
+
+    @GetMapping("/notes")
+    public ResponseEntity<List<Note>> getAllNotes(Pageable pageable) {
+        return ResponseEntity.ok(noteRepository.findAll(pageable)
+                                               .getContent());
+    }
+
+    @GetMapping("/notes/{id}")
+    public ResponseEntity<Note> getNoteById(@PathVariable int id) {
+        return noteRepository.findById(id)
+                             .map(ResponseEntity::ok)
+                             .orElse(ResponseEntity.notFound()
+                                                   .build());
+    }
+
+    @PostMapping("/notes")
+    public ResponseEntity<Note> addNote(@RequestBody @Valid Note note) {
+        Note saved = noteRepository.save(note);
+        return ResponseEntity.created(URI.create("/" + saved.getId()))
+                             .body(saved);
+    }
+
+    @PutMapping("/notes/{id}")
+    public ResponseEntity<?> updateNote(@RequestBody @Valid Note toUpdate,
+                                        @PathVariable int id) {
+        if (!noteRepository.existsById(id)) {
+            return ResponseEntity.notFound()
+                                 .build();
+        }
+        toUpdate.setId(id);
+        noteRepository.save(toUpdate);
+        return ResponseEntity.noContent()
+                             .build();
     }
 }
