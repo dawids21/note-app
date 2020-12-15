5c3f55eda4aeab3f3c6baf070c110f049b289c5d
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-15 07:48:59 +0100

    remove rest resource repository

--- src/main/java/xyz/stasiak/noteapp/model/NoteRepository.java
+++
@@ -1,9 +0,0 @@
-package xyz.stasiak.noteapp.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface NoteRepository extends JpaRepository<Note, Integer> {
-
-}
---
+++ src/main/java/xyz/stasiak/noteapp/model/SqlNoteRepository.java
@@ -0,0 +1,9 @@
+package xyz.stasiak.noteapp.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SqlNoteRepository extends JpaRepository<Note, Integer> {
+
+}
